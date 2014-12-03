7708e9a015797ce65719b5836c28f7bee0c4a372
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2014-12-03 13:01:22 -0200

    adds sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,12 @@
+class SessionsController < ApplicationController
+  def create
+    user = User.from_omniauth(env['omniauth.auth'])
+    session[:user_id] = user.id
+    redirect_to(params[:RelayState] || '/')
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url, :notice => 'User logged out.'
+  end
+end
