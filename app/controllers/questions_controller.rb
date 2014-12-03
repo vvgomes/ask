21e45b55891c86001c24a0f8208b84772fc3f660
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2014-12-03 15:46:31 -0200

    #1 loads a new question to populate the form

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,7 +4,7 @@ class QuestionsController < ApplicationController
   end
 
   def new
-
+    @question = Question.new(:user => current_user)
   end
 
   def create
--- test/controllers/questions_controller_test.rb
+++ test/controllers/questions_controller_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class QuestionsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    Question.delete_all
+    User.delete_all
+    @user = User.create(:email => 'dude@tw.com')
+    @question = Question.create(:user => @user)
+    session[:user_id] = @user.id
+  end
+
+  test 'displays a form to create a new question' do
+    get :new
+    assert response.code == '200'
+    assert assigns(:question).new_record?
+  end
 end
