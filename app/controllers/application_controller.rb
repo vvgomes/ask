9a6e078f4b1641a2869f77de8424e7aad777e704
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2014-12-03 13:18:29 -0200

    adds basic application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,21 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  helper_method :current_user, :forbid!
+
+  def index
+    authenticate!
+  end
+
+  private
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def authenticate!
+    current_user || redirect_to("/auth/saml?redirectUrl=#{URI::encode(request.path)}")
+  end
+
+  def forbid!
+    render :status => :forbidden, :text => 'You do not have rights to perform this operation.'
+  end
 end
