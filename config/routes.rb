2673cbb9913cf7cc163449871dcac6e6a96155ea
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2014-12-03 13:18:08 -0200

    adds authentication routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  match 'auth/:provider/callback',
+    :to => 'sessions#create',
+    :via => [:get, :post]
+
+  match 'signout',
+    :to => 'sessions#destroy',
+    :as => 'signout',
+    :via => [:get, :post]
+
   resources :users
   resources :questions
   root :to => 'application#index'
