54be18ab209e243925bdf97c9bafc5f40a5dfd74
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2014-12-03 12:54:57 -0200

    adds some dummy contents to controllers

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,1 +1,25 @@
+class QuestionsController < ApplicationController
+  def index
 
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+end
