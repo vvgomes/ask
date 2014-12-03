e21f6de40a58cfc937f73c1119b21143024beb60
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2014-12-03 09:39:07 -0200

    adds question model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141203112841) do
+ActiveRecord::Schema.define(version: 20141203113726) do
+
+  create_table "questions", force: true do |t|
+    t.text     "description"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email"
