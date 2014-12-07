9ac3cf30904a13e4622ea7f6872549df2825ee85
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2014-12-07 20:07:02 -0200

    adds schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141203113726) do
+ActiveRecord::Schema.define(version: 20141207213545) do
+
+  create_table "likes", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "question_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "questions", force: true do |t|
     t.text     "description"
