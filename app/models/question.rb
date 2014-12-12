4384c82cb3c0a9b42f9d69f7a4142ce031fb4bd0
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2014-12-12 10:20:03 -0200

    downcase tags

--- app/models/question.rb
+++ app/models/question.rb
@@ -6,4 +6,12 @@ class Question < ActiveRecord::Base
   validates :description, :presence => true
 
   acts_as_taggable
+
+  before_save :downcase_tags
+
+  private
+  
+  def downcase_tags
+    self.tag_list = self.tag_list.map(&:downcase)
+  end
 end
