ea52c724fbf384847b32839d642a55d0d0dbf48c
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2014-12-12 10:04:21 -0200

    breaks lines

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,9 @@
 class Question < ActiveRecord::Base
   belongs_to :user
   has_many :likes
+
   validates :user, :presence => true
   validates :description, :presence => true
+
   acts_as_taggable
 end
