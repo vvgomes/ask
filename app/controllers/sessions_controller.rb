1234044f48467cb1cf6ff2023fca94f89bdd41e0
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2014-12-12 09:49:07 -0200

    no need for a flash here

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_url, :notice => 'User logged out.'
+    redirect_to root_url
   end
 end
