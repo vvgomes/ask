8fa45fc667ce671b4d0ef9348d4eb3515e7f1372
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2015-06-26 17:26:44 -0300

    moving from saml to google auth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,5 @@
 OmniAuth.config.logger = Rails.logger
+
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :saml,
-    :idp_sso_target_url => ENV['IDP_SSO_TARGET_URL'],
-    :idp_cert_fingerprint => ENV['IPD_CERT_FINGERPRINT'],
-    :name_identifier_format => 'urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress',
-    :idp_sso_target_url_runtime_params => { :redirectUrl => :RelayState }
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
 end
